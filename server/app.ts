87a4eda610c49509b36c4600f14be88c3556f582
Matteo <m2lovric@gmail.com>
2020-12-22 17:52:09 +0100

    Add post and delete method

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import {v4} from 'uuid';
 
 interface Blog {
@@ -11,7 +13,12 @@ interface Blog {
 const app = express();
 const PORT = 8000;
 
-const blogs: Blog[] = [
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+let blogs: Blog[] = [
   {id: v4(), title: 'Blog tutorial', content: 'Something', author: 'Matteo'}
 ]
 
@@ -27,6 +34,34 @@ app.get('/blogs/:id', (req, res) => {
   const {id} = req.params;
   const blog = blogs.find(el => el.id === id);
   res.json(blog);
+})
+
+app.post('/blogs', (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  if (!body.title) {
+    return res.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const blogPost: Blog = {
+    id: v4(),
+    title: body.title,
+    content: body.content,
+    author: body.author
+  }
+
+  blogs = blogs.concat(blogPost);
+  res.json(blogPost);
+})
+
+app.delete('/blogs/:id', (req, res) => {
+  const {id} = req.params;
+  blogs = blogs.filter(el => el.id !== id);
+  res.json(blogs);
+  res.status(204).end();
 })
 
 app.listen(PORT, () => {
